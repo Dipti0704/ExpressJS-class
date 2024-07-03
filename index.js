6d5c78db0959a04ad362808af5b2ed2b13195310
Dipti0704 <dipti820h@gmail.com>
2024-07-03 11:53:52 +0530

    class completed

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 // nodemon index.js command
 
 const express = require('express');
-const app = express(); app.use(express.json()); let courses = [
-{id: 1, name: "java"},
-{id: 2, name: "javascript"},
-{id: 3, name: "python"}
-]; app.get('/courses', (req, res) => {
-res.json(courses);
-}); app.post('/courses', (req, res) => {
-console.log(req.body);
-let singleCourse = {
-id: courses.length + 1,
-name: req.body.name
+
+const app = express();
+
+app.use(express.json());
+app.use(loggerMiddleware);
+
+let courese = [
+    {id: 1, name: "John", email: "john@example.com"},
+    {id: 2, name: "John", email: "john@example.com"},
+    {id: 3, name: "Johan", email: "john@example.com"}
+]
+
+app.get('/', (req, res) => {
+        res.json(courese);
+    }
+);
+
+app.post('/', (req, res) => {
+    let singleCourse = {
+        id: courese.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    }
+    courese.push(singleCourse);
+    res.json(courese);
 }
-courses.push(singleCourse);
-res.send(courses);
-}) app.listen(3000, () => {
-console.log("server started")
-});
+);
+
+app.put('/:id', (req, res) => {
+    let course = courese.find(c => c.id === parseInt(req.params.id));
+
+    course.name = req.body.name;
+    course.email = req.body.email;
+    res.json(course);
+
+}
+);
+
+app.delete('/:id', (req, res) => {
+    let course = courese.find(c => c.id === parseInt(req.params.id));
+    let index = courese.indexOf(course);
+    courese.splice(index, 1);
+    res.json(courese);
+}
+);
+
+function sampleMiddleware(req, res, next) {
+    console.log('This is a sample middleware');
+    next();
+}
+
+// contains method, ip, hostname, date
+function loggerMiddleware(req, res, next) {
+    console.log("method: " + req.method);
+    console.log("ip: " + req.ip);
+    console.log("hostname: " + req.hostname);
+    console.log("date: " + new Date());
+    next();
+}
+
+
+app.listen(3000,
+    () => console.log('Listening on port 3000...')
+)
